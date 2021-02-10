0664653767907483388b62e4e031b63bdf87e0f0
brisingr8 <almumo93@gmail.com>
2021-02-10 20:50:43 +0100

    Add files via upload

---
+++ CCuenta.java
@@ -0,0 +1,124 @@
+package cuentas;
+/**
+ * Esta clase nos ayudará a crear y realizar gestiones con una cuenta bancaria.
+ * 
+ * @author alvaro
+ * @version 1.5
+ * @since 1.0
+ */
+public class CCuenta {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+
+    /**
+     * Permite crear la cuenta con los datos pasados por parámetro desde Main
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    /**
+     * Retorna el saldo desde el método getSaldo();
+     */
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    /**
+     * Ingresa cantidades positivas
+     * @param cantidad
+     * @throws Exception 
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * Retira cantidades positivas menores que el saldo disponible. 
+     * @param cantidad
+     * @throws Exception 
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa.");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+}
